37ba3f095bd86b26d4dbdba7a0cc2bd36ec30c93
matvejcekasov <chekashovmatvey@gmail.com>
2024-11-24 21:51:41 +0300

    feat: добавлена Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+from datetime import datetime
+
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(48), unique=True)
+    login = db.Column(db.String(32), unique=True)
+    password = db.Column(db.String(64))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/index')
@@ -14,7 +31,18 @@ def register():
         email = request.form.get('email')
         username = request.form.get('login')
         password = request.form.get('password')
-        print(email, username, password)
+
+        # CRUD:
+        # C - Create
+        # R - Read
+        # U - Update
+        # D - Delete
+
+        # Создание данных
+        user = User(email=email, login=username, password=password)
+
+        db.session.add(user)
+        db.session.commit()
     return render_template('register.html')
 
 
@@ -23,7 +51,24 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        print(email, password)
+
+        # Чтение данных
+        user = User.query.filter_by(email=email, password=password).first()
+        if user:
+            print(f'Успешная авторизация! {user.email}, {user.password}')
+            return render_template('index.html')
+
+        # Обновление данных
+        # user.email = 'admin@gmail.com'
+        # db.session.add(user)
+        # db.session.commit()
+
+        # Удаление данных
+        # db.session.delete(user)
+        # db.session.commit()
+
+        print('Ошибка авторизации!')
+
     return render_template('login.html')
 
 
